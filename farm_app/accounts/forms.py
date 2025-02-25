555cd9a390d8bfe8bc545eef3f36c67987bf6046
Sadi Ahmedova <sadiahmedova@Sadis-MacBook-Pro.local>
2025-02-25 16:26:55 +0200

    Update login page

--- farm_app/accounts/forms.py
+++ farm_app/accounts/forms.py
@@ -23,14 +23,14 @@ class CreateProfileForm(auth_forms.UserCreationForm):
         fields = ['first_name', 'last_name', 'username','gender', 'date_of_birth', 'email','profile_picture', 'password1', 'password2']
 
         widgets = {
-            'first_name': forms.TextInput(attrs={'id':0,'class': 'form-field', 'required': True}),
-            'last_name': forms.TextInput(attrs={'id':1, 'class': 'form-field', 'required': True}),
-            'username': forms.TextInput(attrs={'id':2,'class': 'form-field', 'required': True}),
+            'first_name': forms.TextInput(attrs={'id':0,'class': 'form-field', 'required': True, "placeholder": "e.g., Sadi"}),
+            'last_name': forms.TextInput(attrs={'id':1, 'class': 'form-field', 'required': True, "placeholder": "e.g., Ahmedova"}),
+            'username': forms.TextInput(attrs={'id':2,'class': 'form-field', 'required': True, "placeholder": "e.g., sadiA123"}),
             'date_of_birth': forms.DateInput(
                 attrs={'class': 'form-field date-input', 'type': 'date', 'required': True, 'max': date.today()}),
 
             'gender': forms.Select(attrs={'id':3, 'class': 'form-field'}),
-            'email': forms.EmailInput(attrs={'id':4, 'class': 'form-field'}),
+            'email': forms.EmailInput(attrs={'id':4, 'class': 'form-field', "placeholder": "e.g., sadiahmedova02@gmail.com"}),
         }
 
 
@@ -44,7 +44,7 @@ class LoginProfileForm(auth_forms.AuthenticationForm):
     username = auth_forms.UsernameField(
         widget=forms.TextInput(
             attrs={"autofocus": True,
-                   "class": 'form-field', }))
+                   "class": 'form-field', "placeholder": "e.g., sadiA123"}))
     password = forms.CharField(
         strip=False,
         widget=forms.PasswordInput(
