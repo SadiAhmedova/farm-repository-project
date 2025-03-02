49b9ce277b214f1c3f03fd158383ea873aea71bc
Sadi Ahmedova <sadiahmedova@Sadis-MacBook-Pro.local>
2025-03-02 17:06:33 +0200

    Update navbar for smaller screens

--- static/functions.js
+++ static/functions.js
@@ -148,9 +148,11 @@ document.addEventListener("DOMContentLoaded", function () {
     const menuIcon = document.querySelector(".menu-icon");
     const navbarLinks = document.querySelector(".navbar-links");
 
-    menuIcon.addEventListener("click", function () {
-        navbarLinks.classList.toggle("active");
-    });
+    if (menuIcon) {
+        menuIcon.addEventListener("click", function () {
+            navbarLinks.classList.toggle("active");
+        });
+    }
 });
 
 
